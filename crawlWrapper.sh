67399697ecb3e62d000aff7da66538f0e6c676bc
Nadav Peer <66296636+Nadavpe@users.noreply.github.com>
2021-11-17 09:40:13 +0200

    Task/infra 559/add crawl url (#19)
    * Add crawl URL  (see: https://apester.atlassian.net/browse/INFRA-559)
    
    * Add crawl URL
    
    * Feature/no jira/automation tasks (#18)
    
    * feature: added jobs api
    
    * chore: use report path
    
    * chore: removed hardcoded slack
    
    Co-authored-by: OmriKeret <omri.keret@gmail.com>

---
+++ crawlWrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function die {
+  echo $1
+  exit 1
+}
+
+printf 'Content-type: text/html\n\n'
+
+for p in ${QUERY_STRING//&/ };do kvp=( ${p/=/ } ); k=`echo ${kvp[0]} | awk '{ print toupper($0) }'`; v=${kvp[1]};eval $k=$v;done
+
+[ -z "$JOB" ] && die "Missing job name"
+
+cd /app
+git checkout -f master
+git pull > /dev/null
+
+node cypress-run-crawl-jobs.js $JOB
